3ab058cf14535473302c7fe74824d5a478d2dc50
abrockmeier <alexander.brockmeier@ambicotech.com>
2019-03-10 20:34:58 +0100

    added encoding files added function restructured code TODO: implement function that does not change the argument of the list itself.

--- scratch.py
+++ scratch.py
@@ -1,23 +1,28 @@
-liste = [1,2,3]
-liste_original = [1,2,3]
-
-print(liste)
 
 
-def times_list(n, list):
-    for index, value in enumerate(list):
-        list[index] = n * value
-    return liste
+def print_list_and_type(list_):
+    print()
+    print(list_)
+    print("Type of (%s) is :" % list_)
+    print(type(list_))
+    print()
 
 
-liste = times_list(2, liste)
+def times_list(n, list_):
+    for index, value in enumerate(list_):
+        list_[index] = n * value
+    return list_
 
 
-print()
-print(liste)
+my_list = [1, 2, 3]
+my_list2 = times_list(2, [1, 2, 3])
+my_list3 = times_list(3, [1, 2, 3])
 
 # Wrong output on first and last List-Element!
-listsOFlists = [liste_original], [liste], [times_list(2,liste_original)]
+lists_of_lists = [my_list, my_list2, my_list3]
 
-print()
-print(listsOFlists)
+print_list_and_type(my_list)
+print_list_and_type(my_list2)
+print_list_and_type(my_list3)
+print_list_and_type(lists_of_lists)
+
